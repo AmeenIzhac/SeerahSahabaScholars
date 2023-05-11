36e2c9a031b392868e9f0ed91d1fc7c0f2f08c40
AmeenIzhac <ameen.izhac@gmail.com>
2023-05-11 20:13:23 +0100

    dw

--- next.config.js
+++ next.config.js
@@ -1,6 +1,29 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.worker\.(c|m)?js$/i,
+      use: [
+        {
+          loader: 'worker-loader',
+          options: {
+            name: 'static/[hash].worker.js',
+            publicPath: '/_next/',
+          },
+        },
+        {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      ],
+    });
+    return config;
+  },
 }
 
 module.exports = nextConfig
+
